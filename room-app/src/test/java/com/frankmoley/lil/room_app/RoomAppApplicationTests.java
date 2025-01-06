164f2084589fe46ec8c8edbe15e31fd88cf1cce5
Frank P Moley III <frankmoley@gmail.com>
2025-01-06 04:57:21 +0000

    adding files for 03_03

---
+++ room-app/src/test/java/com/frankmoley/lil/room_app/RoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.room_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

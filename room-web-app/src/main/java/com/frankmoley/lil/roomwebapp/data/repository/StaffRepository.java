55de48f3f9193ace4dc6732438271a546c143537
fpmoles <frankmoley@gmail.com>
2025-01-09 01:30:35 -0600

    fixing repository

--- room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/data/repository/StaffRepository.java
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/data/repository/StaffRepository.java
@@ -2,6 +2,7 @@ package com.frankmoley.lil.roomwebapp.data.repository;
 
 import com.frankmoley.lil.roomwebapp.data.entity.StaffMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface StaffRepository extends JpaRepository<StaffMember, String> {
+public interface StaffRepository extends JpaRepository<StaffMember, UUID> {
 }

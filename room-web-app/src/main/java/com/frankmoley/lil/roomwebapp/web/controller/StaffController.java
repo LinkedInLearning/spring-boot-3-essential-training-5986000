3a1b0a1f7050e3f6726fcf601d0bf8d51a4646e3
fpmoles <frankmoley@gmail.com>
2025-01-09 01:40:31 -0600

    fixing repository

--- room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/StaffController.java
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/StaffController.java
@@ -1,15 +1,10 @@
 package com.frankmoley.lil.roomwebapp.web.controller;
 
-import com.frankmoley.lil.roomwebapp.data.entity.StaffMember;
-import com.frankmoley.lil.roomwebapp.data.repository.StaffRepository;
 import com.frankmoley.lil.roomwebapp.service.StaffService;
-import com.frankmoley.lil.roomwebapp.web.model.Staff;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/staff")

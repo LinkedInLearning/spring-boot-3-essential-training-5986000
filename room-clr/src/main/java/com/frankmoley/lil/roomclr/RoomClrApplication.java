3fca2682b87cfa27df827b62c00a7965521dd9f1
fpmoles <frankmoley@gmail.com>
2025-01-09 23:06:38 -0600

    files for 05_05

--- room-clr/src/main/java/com/frankmoley/lil/roomclr/RoomClrApplication.java
+++ room-clr/src/main/java/com/frankmoley/lil/roomclr/RoomClrApplication.java
@@ -1,5 +1,12 @@
 package com.frankmoley.lil.roomclr;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,18 +25,45 @@ public class RoomClrApplication {
     SpringApplication.run(RoomClrApplication.class, args);
   }
 
+  private static final String QUEUE_NAME = "room-cleaner";
+  private static final String EXCHANGE_NAME = "operations;";
+
+  @Bean
+  public Queue queue(){
+    return new Queue(QUEUE_NAME, false);
+  }
+
+  @Bean
+  public TopicExchange exchange(){
+    return new TopicExchange(EXCHANGE_NAME);
+  }
+
+  @Bean
+  public Binding binding(Queue queue, TopicExchange exchange){
+    return BindingBuilder.bind(queue).to(exchange).with("landon.#");
+  }
+
   @Bean
   public RestTemplate restTemplate(RestTemplateBuilder builder){
     return builder.build();
   }
 
   @Bean
-  public CommandLineRunner run(RestTemplate restTemplate) {
+  public CommandLineRunner run(RestTemplate restTemplate, RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
     return args -> {
       ResponseEntity<List<Room>> rooms = restTemplate.exchange("http://localhost:8080/api/rooms",
           HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {
           });
-      rooms.getBody().forEach(System.out::println);
+      rooms.getBody().forEach(r -> {
+        AsyncPayload payload = new AsyncPayload();
+        payload.setId(r.getId());
+        payload.setModel("ROOM");
+        try{
+          rabbitTemplate.convertAndSend("operations", "landon.rooms.cleaner", objectMapper.writeValueAsString(payload));
+        }catch (JsonProcessingException e){
+          throw new RuntimeException(e);
+        }
+      });
     };
   }
 
